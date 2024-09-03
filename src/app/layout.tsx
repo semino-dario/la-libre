7eefe2421aedfb147ee649a475d5445f1b0b4bc1
Dario Semino <dariomartinsemino@gmail.com>
2024-09-03 15:31:47 -0300

    thumbnail red

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,24 @@ import styles from './page.module.scss'
 export const metadata: Metadata = {
   title: "La Libre - Cooperativa de libros y cultura",
   description: "Librería alternativa y centro cultural, ubicada en el barrio de San Telmo, en Buenos Aires.",
+  openGraph: {
+    title: "La Libre - Cooperativa de libros y cultura",
+    description: "Librería alternativa y centro cultural, ubicada en el barrio de San Telmo, en Buenos Aires.",
+    images: [
+      {
+        url: "https://la-libre.vercel.app/thumbnail.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Descripción de la imagen",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "La Libre - Cooperativa de libros y cultura",
+    description: "Librería alternativa y centro cultural, ubicada en el barrio de San Telmo, en Buenos Aires.",
+    images: ["https://la-libre.vercel.app/thumbnail.jpg"],
+  },
 };
 
 export default function RootLayout({
