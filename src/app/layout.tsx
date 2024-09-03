1cdfe3bc97a11b2bf9d0fd02124930c7e9c69c7e
Dario Semino <dariomartinsemino@gmail.com>
2024-09-03 15:41:22 -0300

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body className={styles.main}>{children}</body>
     </html>
   );
