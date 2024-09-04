faf5c708e1b4ba1d29a90207bf4374f09c9dbfd4
Dario Semino <dariomartinsemino@gmail.com>
2024-09-04 12:43:24 -0300

    medatada

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,23 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-      <link rel="icon" href="/favicon.ico" sizes="any" />
+        <title>La Libre - Cooperativa de libros y cultura</title>
+        <meta name="description" content="Librería alternativa y centro cultural, ubicada en el barrio de San Telmo, en Buenos Aires." />
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:title" content="La Libre - Cooperativa de libros y cultura" />
+        <meta property="og:description" content="Librería alternativa y centro cultural, ubicada en el barrio de San Telmo, en Buenos Aires." />
+        <meta property="og:image" content="https://la-libre.vercel.app/thumbnail.jpg" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:image:alt" content="Descripción de la imagen" />
+        <meta property="og:url" content="https://la-libre.vercel.app/" />
+        <meta property="og:type" content="website" />
+        {/* <!-- Twitter --> */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="La Libre - Cooperativa de libros y cultura" />
+        <meta name="twitter:description" content="Librería alternativa y centro cultural, ubicada en el barrio de San Telmo, en Buenos Aires." />
+        <meta name="twitter:image" content="https://la-libre.vercel.app/thumbnail.jpg" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
       <body className={styles.main}>{children}</body>
     </html>
