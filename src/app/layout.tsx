ab7a8583abddb4fc83fadc277a0bb06bdb24f6fb
Dario Semino <dariomartinsemino@gmail.com>
2024-09-04 11:29:32 -0300

    favicon correct

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
       <body className={styles.main}>{children}</body>
     </html>
